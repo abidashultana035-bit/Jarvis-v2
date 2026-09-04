819e9fa0b2086f00c7c04fad8b97e6a4d8c3012f
abidashultana035-bit <abidashultana035@gmail.com>
2026-09-04 10:20:53 +0600

    Create MarketingTrends.java

---
+++ app/src/main/java/com/jarvis/v3/commands/MarketingTrends.java
@@ -0,0 +1,9 @@
+package com.jarvis.v3.commands;
+public class MarketingTrends {
+    public static String getTrends(){
+        return "Today trending Sir: AI, Share Market up, Crypto... I am learning continuously Sir";
+    }
+    public static String shareMarketIdea(String topicSir){
+        return "Marketing idea for "+topicSir+" Sir: Use AI reels + trending hashtags Sir";
+    }
+}
