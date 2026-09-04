ce0b3b1d1aa8f1797dbf32136c7258db1829a36f
abidashultana035-bit <abidashultana035@gmail.com>
2026-09-04 10:20:21 +0600

    Create CodingEngine.java

---
+++ app/src/main/java/com/jarvis/v3/commands/CodingEngine.java
@@ -0,0 +1,7 @@
+package com.jarvis.v3.commands;
+public class CodingEngine {
+    public static String doCoding(String requestSir){
+        // Self Learner coding - offline e template, online e AI code gen Sir
+        return "Coding Sir for: "+requestSir+"\n\n```code\n// JARVIS v3 auto generated Sir\nSystem.out.println(\"Hello Sir\");\n```\nSir code ready Sir";
+    }
+}
