63c4905d9c06f668d7e2d1d4e417ba4044cbb070
abidashultana035-bit <abidashultana035@gmail.com>
2026-09-07 21:50:52 +0600

    Update VoiceLock.java

--- app/src/main/java/com/jarvis/v3/voice/VoiceLock.java
+++ app/src/main/java/com/jarvis/v3/voice/VoiceLock.java
@@ -1,4 +1,4 @@
-package com.jarvis.v3.voice;
+package com.jarvis.v2.voice;
 import java.util.*;
 public class VoiceLock {
     public static String ownerVoicePrint = null; // First time apnar voice save hobe Sir
