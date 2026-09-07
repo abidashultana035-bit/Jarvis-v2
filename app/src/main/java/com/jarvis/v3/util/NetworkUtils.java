91dbe6da2f48812c0e7fa6eb6106860e934830d9
abidashultana035-bit <abidashultana035@gmail.com>
2026-09-07 21:10:18 +0600

    Create NetworkUtils.java

---
+++ app/src/main/java/com/jarvis/v3/util/NetworkUtils.java
@@ -0,0 +1,14 @@
+package com.jarvis.v3.util;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class NetworkUtils {
+    public static boolean isOnline(Context ctx){
+        try{
+            ConnectivityManager cm = (ConnectivityManager) ctx.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo ni = cm.getActiveNetworkInfo();
+            return ni != null && ni.isConnected();
+        }catch(Exception e){ return false; }
+    }
+}
