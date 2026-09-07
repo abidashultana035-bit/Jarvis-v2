16bdde0dabc4b8b9872e6369b8997a7e9cb3da11
abidashultana035-bit <abidashultana035@gmail.com>
2026-09-07 21:09:12 +0600

    Update IntruderDetector.java

--- app/src/main/java/com/jarvis/v3/security/IntruderDetector.java
+++ app/src/main/java/com/jarvis/v3/security/IntruderDetector.java
@@ -1,18 +1,14 @@
 package com.jarvis.v3.security;
 import android.content.Context;
+import com.jarvis.v3.util.CameraCapture;
+
 public class IntruderDetector {
     public static int failedAttempts = 0;
     public static void onFailedUnlock(Context ctx){
         failedAttempts++;
         if(failedAttempts >= 3){
-            // Selfie tulbe Sir
-            takeSelfie(ctx);
+            CameraCapture.captureFrontSelfie(ctx, "Intruders");
             failedAttempts = 0;
         }
-    }
-    public static void takeSelfie(Context ctx){
-        // Camera diye selfie save korbe Sir - /sdcard/JARVIS/Intruders/
-        com.jarvis.v3.commands.CameraManager.requestCamera("Intruder detection Sir");
-        // Code to capture image Sir
     }
 }
