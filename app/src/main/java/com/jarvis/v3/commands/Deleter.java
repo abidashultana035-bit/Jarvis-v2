21f45fa6095f0de3e16238a706b19200d4dfdf98
abidashultana035-bit <abidashultana035@gmail.com>
2026-09-14 17:02:53 +0600

    Update Deleter.java

--- app/src/main/java/com/jarvis/v3/commands/Deleter.java
+++ app/src/main/java/com/jarvis/v3/commands/Deleter.java
@@ -1,5 +1,5 @@
-package com.jarvis.v3.commands;
-import com.jarvis.v3.brain.Memory;
+package com.jarvis.v2.commands;
+import com.jarvis.v2.brain.Memory;
 public class Deleter {
     public static String deleteData(String whatSir){
         Memory.knowledge.remove(whatSir.toLowerCase());
